f45f771422df326a508959af7fcb57f751db87df
Muhammad Abdulwahid <m.abdulwahid008@gmail.com>
2023-12-21 00:32:40 +0500

    commit

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,32 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const admin = '0xA785b969469D1A5e752E82d39d69668F5A327e6D'
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const governmentContract = await ethers.deployContract("Government", [admin]);
+  const govReceipt = await governmentContract.waitForDeployment();
+  const governmentAddress = govReceipt.target;
+  console.log("Government Address is "+ governmentAddress);
+  
 
-  await lock.waitForDeployment();
+  const companyContract = await ethers.deployContract("Company", [governmentAddress]);
+  const companyReceipt = await companyContract.waitForDeployment();
+  const companyAddress = companyReceipt.target;
+  console.log("Company Address is "+ companyAddress);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+
+  const VRSContract = await ethers.deployContract("VRS", [companyAddress, governmentAddress]);
+  const VRSReceipt = await VRSContract.waitForDeployment();
+  const VRSAddress = VRSReceipt.target;
+  console.log("VRS Address is "+ VRSAddress);
+  
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
